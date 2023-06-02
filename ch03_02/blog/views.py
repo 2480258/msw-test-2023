bf69c0638201e19a038ce5ad1a2e714b97009f9d
2480258 <swh8059@naver.com>
2023-06-02 05:03:26 +0000

    test

--- ch03_02/blog/views.py
+++ ch03_02/blog/views.py
@@ -6,7 +6,7 @@ from .forms import PostForm
 from django.contrib.auth.models import Group, Permission, User
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
--- ch03_02/mysite/urls.py
+++ ch03_02/mysite/urls.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url('api-token-auth/', obtain_auth_token), #added
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
